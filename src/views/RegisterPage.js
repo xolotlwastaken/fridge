bd026b96c6ea1c645cc0588e4979f4829d75548f
xolotlwastaken <xolotlwastaken@gmail.com>
2024-08-07 12:33:40 +0800

    added login and register functionality

--- src/views/RegisterPage.js
+++ src/views/RegisterPage.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [error, SetError] = useState("");
+  const navigate = useNavigate();
 
   return (
     <Container>
@@ -17,24 +23,44 @@ export default function RegisterPage() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Password"
+            placeholder="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
           <a href="/login">Have an existing account? Login here.</a>
         </Form.Group>
-        <Button variant="primary">Sign Up</Button>
+
+        <Button variant="primary" onClick={async (e) => {
+            SetError("");
+            const canSignup = username && password && password===confirmPassword;
+            if (canSignup) {
+                try{
+                    await createUserWithEmailAndPassword(auth, username, password);
+                    navigate("/");
+                } catch (error) {
+                    SetError(error.message);
+                }
+            }
+        }
+        }>Sign Up</Button>
       </Form>
-      <p></p>
+      <p>error</p>
     </Container>
   );
 }
